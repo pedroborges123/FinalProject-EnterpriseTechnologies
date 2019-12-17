fdcc4d8b6e4761f1ac5a46335b3569d925b875fc
DESKTOP-Infobase\Pedro <pedro.borges@infobase.com.br>
2019-12-16 20:14:07 -0500

    remove garbage and fixing the footer

--- front-end/app/assets/script/scripts.js
+++ front-end/app/assets/script/scripts.js
@@ -11,14 +11,6 @@ jQuery(document).ready(function () {
 
 function build_ui() {
 
-    /*{"_id":"5ddd6f3bd2b0da2d347bc221",
-        "Name":"Leonidas",
-        "LastName":"Pereira",
-        "RegistrationDate":"11-09-2016",
-        "Co-op":"Y",
-        "GPA":3.2,
-        "GraduationDate":"20-04-2019"} */
-
     let student_collection_fields = [
         new CollectionField('ID', '_id', 'text', false, 'disabled="disabled"'),
         new CollectionField('Name', 'Name', 'text', true),
@@ -29,21 +21,10 @@ function build_ui() {
         new CollectionField("Graduation Date", 'GraduationDate', 'date', false)
     ];
 
-    let student_collection_generator = new CollectionGenerator('Cestar_C0740134', student_collection_fields);
+    let student_collection_generator = new CollectionGenerator('Pedro_C0740134', student_collection_fields);
     student_collection_generator.handle_ui();// For all collections
     student_collection_generator.set_up_modal();// Only for the first on collection
     student_collection_generator.load_table();// Only for the first on collection
-
-    // TODO: Remove this...
-    /*let cool_collection_fields = [
-        new CollectionField('ID', '_id', 'text', false, 'disabled="disabled"'),
-        new CollectionField('Name', 'Name', 'text', true),
-        new CollectionField('Last Name', 'LastName', 'text', true),
-        new CollectionField('Rating', 'rating', 'number', true)];
-
-    let cool_collection_generator = new CollectionGenerator('Cool_List', cool_collection_fields);
-    cool_collection_generator.handle_ui();// For all collections
-*/
 
     // Flavio Noe...
     let flavio_collection_fields = [
